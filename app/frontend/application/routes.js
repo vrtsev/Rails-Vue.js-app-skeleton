c27ed4b7898e51a9be7abac7151877e6c8a322d3
Vadim Riazantsev <v.riazantsev@mobidev.biz>
2018-06-19 22:49:13 +0300

    Create frontend folder with Vue.js initializer

---
+++ app/frontend/application/routes.js
@@ -0,0 +1,20 @@
+// DEVELOPMENT INFO
+// Use '/path/:id' for dynamic segments
+// dynamic segments == $route.params
+
+// Named route:
+// path: '/user/:userId',
+// <router-link :to="{ name: 'user', params: { userId: 123 }}">Пользователь</router-link>
+// router.push({ name: 'user', params: { userId: 123 }})
+
+// =============================================================
+// APPLICATION ROUTES
+// =============================================================
+// Main component
+
+export default [
+]
+
+// Development info
+// 'this.$route' - to get access to current route
+
---
+++ app/frontend/application/store.js
@@ -0,0 +1,6 @@
+// You can access each module in console by typing 'store.state.example'
+const store = {
+  modules: {}
+}
+
+export default store
